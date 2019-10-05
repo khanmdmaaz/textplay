34f52cce9232094af635250f7dca648fdc0df1f9
Mohd. Maaz Khan <khanmdmaaz@gmail.com>
2019-10-05 21:31:18 +0530

    Update settings.py

--- textplay/settings.py
+++ textplay/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ssbgkwq=jq6yfe(67q1yb32lu@cn*e@e^xz&rs4rwtb(yvne45'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
